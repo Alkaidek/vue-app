96d353c78f718a4a447acba13fd66ae609ce4d6b
Alkaidek <35803025+Alkaidek@users.noreply.github.com>
2018-09-28 14:23:48 +0200

    viuziummmmm

--- src/components/uiComponent.vue
+++ src/components/uiComponent.vue
@@ -3,6 +3,10 @@
         <div id="menuBox">
             <div id="corner"></div>
             <div id="corner2"></div>
+            <div id="corner3"></div>
+            <div id="corner4"></div>
+            <div id="corner5"></div>
+            <div id="corner6"></div>
             <md-button class="md-raised"  v-on:click="changeRound">{{txt}}</md-button>
             <div id="menu">
                 <div id="menuCircle"></div>
@@ -36,7 +40,28 @@
                    document.getElementById('corner').style.transform = 'rotate(45deg)';
                    document.getElementById('corner2').style.borderRadius = '0%';
                    document.getElementById('corner2').style.transform = 'rotate(45deg)';
-                   this.txt =  'Edge of the World!';
+                   document.getElementById('corner3').style.borderRadius = '0%';
+                   document.getElementById('corner3').style.transform = 'rotate(45deg)';
+                   document.getElementById('corner4').style.borderRadius = '0%';
+                   document.getElementById('corner4').style.transform = 'rotate(45deg)';
+                   document.getElementById('corner5').style.borderRadius = '0%';
+                   document.getElementById('corner5').style.transform = 'rotate(45deg)';
+                   document.getElementById('corner6').style.borderRadius = '0%';
+                   document.getElementById('corner6').style.transform = 'rotate(45deg)';
+                   /*464619
+                   461912*/
+                   document.getElementById('menuRound').style.backgroundColor = '#464619';
+                   document.getElementById('menuRound').style.backgroundColor = '#464619';
+                   document.getElementById('menuBox').style.backgroundColor = '#464619';
+                   document.getElementById('menu').style.backgroundColor = ' #461912';
+                   document.getElementById('menuCircle').style.backgroundColor = '#464619';
+                   document.getElementById('corner').style.backgroundColor = '#464619';
+                   document.getElementById('corner2').style.backgroundColor = '#464619';
+                   document.getElementById('corner3').style.backgroundColor = '#461912';
+                   document.getElementById('corner4').style.backgroundColor = '#461912';
+                   document.getElementById('corner5').style.backgroundColor = '#464619';
+                   document.getElementById('corner6').style.backgroundColor = '#464619';
+                   this.txt =  'The world is gonna rolle me!';
                } else {
                    document.getElementById('menuRound').style.borderRadius = '50%';
                    document.getElementById('menuRound').style.transform = 'rotate(0deg)';
@@ -47,7 +72,27 @@
                    document.getElementById('corner2').style.transform = 'rotate(0deg)';
                    document.getElementById('corner').style.borderRadius = '50%';
                    document.getElementById('corner').style.transform = 'rotate(0deg)';
-                   this.txt =  'The world is gonna rolle me!';
+                   document.getElementById('corner3').style.borderRadius = '50%';
+                   document.getElementById('corner3').style.transform = 'rotate(0deg)';
+                   document.getElementById('corner4').style.borderRadius = '50%';
+                   document.getElementById('corner4').style.transform = 'rotate(0deg)';
+                   document.getElementById('corner5').style.borderRadius = '50%';
+                   document.getElementById('corner5').style.transform = 'rotate(0deg)';
+                   document.getElementById('corner6').style.borderRadius = '50%';
+                   document.getElementById('corner6').style.transform = 'rotate(0deg)';
+
+                   document.getElementById('menuRound').style.backgroundColor = '#461912';
+                   document.getElementById('menuRound').style.backgroundColor = '#461912';
+                   document.getElementById('menuBox').style.backgroundColor = '#461912';
+                   document.getElementById('menu').style.backgroundColor = ' #464619';
+                   document.getElementById('menuCircle').style.backgroundColor = '#461912';
+                   document.getElementById('corner').style.backgroundColor = '#461912';
+                   document.getElementById('corner2').style.backgroundColor = '#461912';
+                   document.getElementById('corner3').style.backgroundColor = '#464619';
+                   document.getElementById('corner4').style.backgroundColor = '#464619';
+                   document.getElementById('corner5').style.backgroundColor = '#461912';
+                   document.getElementById('corner6').style.backgroundColor = '#461912';
+                   this.txt =  'Edge of the World!';
                }
                this.round = !this.round;
            }
@@ -70,6 +115,7 @@
         border-radius: 10%;
         transition: .6s;
         transition-delay: .4s;
+        z-index: 1;
     }
     #menuRound {
         background-color: #461912;
@@ -79,7 +125,7 @@
         border-radius: 50%;
         margin-left: 45%;
         transition: .6s;
-        z-index: 9;
+        z-index: 0;
     }
     #menu {
         width: 10vw;
@@ -88,7 +134,6 @@
         margin-left: 40%;
         margin-top: 10%;
         border-radius: 50%;
-        opacity: 0.6;
         transition: .6s;
         transition-delay: .8s;
         z-index: 10;
@@ -100,7 +145,6 @@
         background-color: #461912;
         margin-left:30%;
         border-radius: 50%;
-        opacity: 1;
         transition: .6s;
         transition-delay: .6s;
     }
@@ -127,6 +171,52 @@
         height: 5vw;
         margin-left: 47.5vw;
         margin-top: 5vw;
+        position: absolute;
+        border-radius: 50%;
+        background-color: #461912;
+        transition: .6s;
+        transition-delay: 1.2s;
+    }
+    #corner4 {
+        width:5vw;
+        height: 5vw;
+        margin-left: 47.5vw;
+        margin-top: 10vw;
+        position: absolute;
+        border-radius: 50%;
+        background-color:  #464619;
+        transition: .6s;
+        transition-delay: 1.2s;
+        z-index: 11;
+    }
+    #corner3 {
+        width:5vw;
+        height: 5vw;
+        margin-left: -2.5vw;
+        margin-top: 10vw;
+        position: absolute;
+        border-radius: 50%;
+        background-color:  #464619;
+        transition: .6s;
+        transition-delay: 1.2s;
+        z-index: 11;
+    }
+    #corner5 {
+        width:5vw;
+        height: 5vw;
+        margin-left: 47.5vw;
+        margin-top: 15vw;
+        position: absolute;
+        border-radius: 50%;
+        background-color: #461912;
+        transition: .6s;
+        transition-delay: 1.2s;
+    }
+    #corner6 {
+        width:5vw;
+        height: 5vw;
+        margin-left: -2.5vw;
+        margin-top: 15vw;
         position: absolute;
         border-radius: 50%;
         background-color: #461912;
