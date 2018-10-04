1243300d9b5825351e7684a7431253a4b3bd9626
Alkaidek <35803025+Alkaidek@users.noreply.github.com>
2018-10-04 13:45:06 +0200

    VUExit

--- src/components/photoGrid.vue
+++ src/components/photoGrid.vue
@@ -3,123 +3,164 @@
         <div id="grid-box">
             <div>
                 <img id="img1" src="../assets/img/1.jpg" v-on:click="setImg()">
+                <h1>szynka</h1>
             </div>
             <div>
                 <img id="img2" src="../assets/img/2.jpg" v-on:click="setImg()">
+                <h1>bekon</h1>
             </div>
             <div>
                 <img id="img3" src="../assets/img/3.jpg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img4" src="../assets/img/4.jpg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img5" src="../assets/img/5.jpeg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img6" src="../assets/img/6.jpg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img7" src="../assets/img/7.jpg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img8" src="../assets/img/8.jpeg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img9" src="../assets/img/9.jpg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img10" src="../assets/img/3.jpg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img11" src="../assets/img/1.jpg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img12" src="../assets/img/2.jpg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img13" src="../assets/img/3.jpg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img14" src="../assets/img/4.jpg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img15" src="../assets/img/5.jpeg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img16" src="../assets/img/6.jpg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img17" src="../assets/img/7.jpg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img18" src="../assets/img/8.jpeg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img19" src="../assets/img/9.jpg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img20" src="../assets/img/4.jpg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img21" src="../assets/img/1.jpg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img22" src="../assets/img/2.jpg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img23" src="../assets/img/3.jpg" v-on:click="setImg()">
-            </div>
-            <div>
-                <img id="img24" src="../assets/img/4.jpg" v-on:click="setImg()">
+                <h1>parówki</h1>
             </div>
             <div>
                 <img id="img25" src="../assets/img/5.jpeg" v-on:click="setImg()">
+                <h1>psztet</h1>
+
             </div>
             <div>
                 <img id="img26" src="../assets/img/6.jpg" v-on:click="setImg()">
+                <h1>kiełbasa</h1>
             </div>
             <div>
                 <img id="img27" src="../assets/img/7.jpg" v-on:click="setImg()">
+                <h1>pasztetowa</h1>
             </div>
             <div>
                 <img id="img28" src="../assets/img/8.jpeg" v-on:click="setImg()">
+                <h1>pulled pork</h1>
             </div>
             <div>
                 <img id="img29" src="../assets/img/9.jpg" v-on:click="setImg()">
+                <h1>salami</h1>
+            </div>
+            <div>
+                <img id="img10" src="../assets/img/3.jpg" v-on:click="setImg()">
+                <h1>burger</h1>
+            </div>
+            <div>
+                <img id="img11" src="../assets/img/1.jpg" v-on:click="setImg()">
+                <h1>kaszanka</h1>
+            </div>
+            <div>
+                <img id="img12" src="../assets/img/2.jpg" v-on:click="setImg()">
+                <h1>karkówka</h1>
+            </div>
+            <div>
+                <img id="img13" src="../assets/img/3.jpg" v-on:click="setImg()">
+                <h1>szpek</h1>
+            </div>
+            <div>
+                <img id="img14" src="../assets/img/4.jpg" v-on:click="setImg()">
+                <h1>wieprzowina</h1>
+            </div>
+            <div>
+                <img id="img5" src="../assets/img/5.jpeg" v-on:click="setImg()">
+                <h1>drób</h1>
+            </div>
+            <div>
+                <img id="img6" src="../assets/img/6.jpg" v-on:click="setImg()">
+                <h1>kebab</h1>
+            </div>
+            <div>
+                <img id="img7" src="../assets/img/7.jpg" v-on:click="setImg()">
+                <h1>wołowina</h1>
+            </div>
+            <div>
+                <img id="img8" src="../assets/img/8.jpeg" v-on:click="setImg()">
+                <h1>pizza</h1>
+            </div>
+            <div>
+                <img id="img9" src="../assets/img/9.jpg" v-on:click="setImg()">
+                <h1>zapiekanka</h1>
+            </div>
+            <div>
+                <img id="img17" src="../assets/img/7.jpg" v-on:click="setImg()">
+                <h1>hot-dog</h1>
+            </div>
+            <div>
+                <img id="img18" src="../assets/img/8.jpeg" v-on:click="setImg()">
+                <h1>lasagne</h1>
+            </div>
+            <div>
+                <img id="img19" src="../assets/img/9.jpg" v-on:click="setImg()">
+                <h1>tortilla</h1>
+            </div>
+            <div>
+                <img id="img20" src="../assets/img/4.jpg" v-on:click="setImg()">
+                <h1>nuggetsy</h1>
+            </div>
+            <div>
+                <img id="img21" src="../assets/img/1.jpg" v-on:click="setImg()">
+                <h1>rosbef</h1>
+            </div>
+            <div>
+                <img id="img22" src="../assets/img/2.jpg" v-on:click="setImg()">
+                <h1>befsztyk</h1>
+            </div>
+            <div>
+                <img id="img15" src="../assets/img/5.jpeg" v-on:click="setImg()">
+                <h1>kotlet</h1>
+            </div>
+            <div>
+                <img id="img16" src="../assets/img/6.jpg" v-on:click="setImg()">
+                <h1>pomidorowa</h1>
+            </div>
+            <div>
+                <img id="img4" src="../assets/img/4.jpg" v-on:click="setImg()">
+                <h1>ser</h1>
+            </div>
+            <div>
+                <img id="img23" src="../assets/img/3.jpg" v-on:click="setImg()">
+                <h1>szaszłyk</h1>
+            </div>
+            <div>
+                <img id="img24" src="../assets/img/4.jpg" v-on:click="setImg()">
+                <h1>cebula</h1>
             </div>
             <div>
                 <img id="img30" src="../assets/img/3.jpg" v-on:click="setImg()">
+                <h1>papryka</h1>
             </div>
             <div>
                 <img id="img31" src="../assets/img/1.jpg" v-on:click="setImg()">
+                <h1>pomidor</h1>
             </div>
             <div>
                 <img id="img32" src="../assets/img/2.jpg" v-on:click="setImg()">
+                <h1>czosnek</h1>
             </div>
             <div>
                 <img id="img33" src="../assets/img/3.jpg" v-on:click="setImg()">
+                <h1>frytki</h1>
             </div>
             <div>
                 <img id="img34" src="../assets/img/4.jpg" v-on:click="setImg()">
+                <h1>paluszki rybne</h1>
             </div>
             <div>
                 <img id="img35" src="../assets/img/5.jpeg" v-on:click="setImg()">
+                <h1>sałatka</h1>
             </div>
             <div>
                 <img id="img36" src="../assets/img/6.jpg" v-on:click="setImg()">
+                <h1>taco</h1>
             </div>
             <div>
                 <img id="img37" src="../assets/img/7.jpg" v-on:click="setImg()">
+                <h1>grochówka</h1>
             </div>
             <div>
                 <img id="img38" src="../assets/img/8.jpeg" v-on:click="setImg()">
+                <h1>dyniowa</h1>
             </div>
             <div>
                 <img id="img39" src="../assets/img/9.jpg" v-on:click="setImg()">
+                <h1>spaghetti</h1>
             </div>
             <div>
                 <img id="img40" src="../assets/img/4.jpg" v-on:click="setImg()">
+                <h1>pesto</h1>
             </div>
         </div>
     </div>
@@ -130,6 +171,7 @@
         name: "photoGrid",
         data: function () {
             return {
+
             }
         },
         methods: {
@@ -137,7 +179,7 @@
                 document.getElementById(event.target.id).style.transform = 'scale(1, 1)';
                 document.getElementById(event.target.id).style.boxShadow = '0 0 0px 0px black';
                 document.getElementById(event.target.id).style.opacity = '1';
-
+                document.getElementById(event.target.id).nextSibling.style.opacity = '1';
             }
         }
     }
@@ -146,30 +188,44 @@
 
 <style scoped>
     h1{
-        z-index: 10;
-        color: red;
+        text-align: center;
+        //mix-blend-mode: difference;
+        color: rgba(196, 196, 196, 0.5);
+        font-size: 50px;
+        //filter: invert(100%);
+       // margin-top: -60px;
+        transition: 1s;
+        opacity: 0;
+        transition-delay: 1s;
+        width: 100%;
+        background-color: rgba(0 ,0 , 0, 0.5);
+        position: relative;
+        height: 60px;
+        padding-top: 20px;
+        margin-top: -60px;
+        padding-bottom: 0;
+        margin-bottom: 0;
     }
     #grid-box {
-        line-height: 0;
-        -webkit-column-count: 5;
-        -webkit-column-gap:   0px;
-        -moz-column-count:    5;
-        -moz-column-gap:      0px;
-        column-count:         5;
-        column-gap:           0px;
+        display: flex;
+        flex-wrap: wrap;
+        align-items: flex-start;
+        flex-direction: column;
+        max-width: 100vw;
+        max-height: 330vh;
+        align-content: space-around;
     }
     #grid-box div {
 /*        border: red 1px solid;
         background-color: gray;*/
     }
     #grid-box div img {
-        width: 100% !important;
+        width: 25vw !important;
         height: auto !important;
         opacity: 0.3;
         transition: 1.5s;
         transform: scale(.6, .6);
-        box-shadow: 0 0 0px 50px black;
-        z-index: 2;
+        box-shadow: 0 0 0 4vh black;
     }
     /*#grid-box div img:active {*/
         /*transform: scale(1, 1);*/
@@ -178,10 +234,8 @@
     /*}*/
     #grid-box div img:hover {
         opacity: 1;
-        z-index: 2;
     }
-
-    @media (max-width: 1200px) {
+   /* @media (max-width: 1200px) {
         #grid-box  {
             -moz-column-count:    4;
             -webkit-column-count: 4;
@@ -208,5 +262,5 @@
             -webkit-column-count: 1;
             column-count:         1;
         }
-    }
+    }*/
 </style>
